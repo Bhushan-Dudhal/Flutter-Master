d79bf151183e12801355976ec9c99423084ece5e
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-10-01 04:29:59 +0530

    button file add

---
+++ button04/android/app/src/main/kotlin/com/example/button04/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.button04
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ button04/lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Button",
+      theme: ThemeData(primarySwatch: Colors.cyan),
+      home: _MyApp(),
+    );
+  }
+}
+
+class _MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Button"),
+        backgroundColor: Colors.blue,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                print('Text Button');
+              },
+              onLongPress: () {
+                print('On Long Press');
+              },
+              child: Text('Click here'),
+            ),
+        
+        //////////   ElevatedButton   //////////////////////////////
+            ElevatedButton(
+              onPressed: () {
+                print('Elevation Button call');
+              },
+              child: Text('Elevation Button'),
+            ),
+        
+        /////////////////////////  Outline Button  ////////////////////////////////////
+            OutlinedButton(
+              onPressed: () {
+                print("Outline Button");
+              },
+              child: Text('Out Line Button'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
