c2f1e31df98a57e4c9a5672ff05c26fac761fa4b
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-10-01 20:55:21 +0530

    image widget

---
+++ image_widget05/lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(title: "Image", home: Screen());
+  }
+}
+
+class Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        backgroundColor: Colors.blueGrey,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          height: 100,
+          width: 100,
+
+          child: Image.asset(
+            'assets/images/life.jpeg',
+            height: 300,
+            width: 300,
+          ),
+        ),
+      ),
+    );
+  }
+}
