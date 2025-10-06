b9642629c8d535aeb8a242a237a9e0a4e5a8e13c
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-10-06 08:29:58 +0530

    istview_builder updated

--- istview_builder10/lib/main.dart
+++ istview_builder10/lib/main.dart
@@ -14,6 +14,7 @@ const MyApp({super.key});
 @override
 Widget build(BuildContext context){
   return MaterialApp(
+    debugShowCheckedModeBanner: false,
     title: "Demo",
     theme: ThemeData(
       primarySwatch: Colors.deepPurple
