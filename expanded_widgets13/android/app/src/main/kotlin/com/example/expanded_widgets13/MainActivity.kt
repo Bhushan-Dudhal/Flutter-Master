3cbbab50f82e3d30aad03ebc394a0b58d4c8a884
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-10-08 07:20:16 +0530

    Expanded Widgets

---
+++ expanded_widgets13/android/app/src/main/kotlin/com/example/expanded_widgets13/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.expanded_widgets13
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ expanded_widgets13/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "New app",
+      theme: ThemeData(primarySwatch: Colors.cyan),
+      home: Screen(),
+    );
+  }
+}
+
+class Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("welcome "), backgroundColor: Colors.blueGrey),
+      body: Column(
+        // mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(flex: 2, child: Container(height: 100, color: Colors.blue)),
+          Expanded(
+            flex: 1,
+
+            child: Container(height: 100, color: Colors.deepOrangeAccent),
+          ),
+          Expanded(
+            flex: 2,
+
+            child: Container(height: 100, color: Colors.deepPurple),
+          ),
+          Expanded(flex: 1, child: Container(height: 100, color: Colors.green)),
+        ],
+      ),
+    );
+  }
+}
