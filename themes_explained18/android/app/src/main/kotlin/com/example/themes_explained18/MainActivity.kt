63d46c82d7afd94d360c33e67e5cf1419d7a70ac
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-10-11 11:01:04 +0530

    Styles and Themes - Explained

---
+++ themes_explained18/android/app/src/main/kotlin/com/example/themes_explained18/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.themes_explained18
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ themes_explained18/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Theme",
+      theme: ThemeData(primarySwatch: Colors.blue,
+
+      textTheme: TextTheme(
+        headlineLarge: TextStyle(fontSize: 20)
+    
+      )
+
+      ),
+      home: Screen(),
+    );
+  }
+}
+
+class Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+        backgroundColor: Colors.pinkAccent,
+      ),
+      body: Column(
+        children: [
+          Text("welcome",style:Theme.of(context).textTheme.headlineLarge),
+          Text("Core2Web",style:Theme.of(context).textTheme.headlineLarge),
+          Text("welcome",style: TextStyle(fontSize: 40,fontFamily: 'FontMain'),),
+          Text("welcome",style: TextStyle(fontSize: 22,fontFamily: 'FontMain',fontWeight: FontWeight.bold),),
+
+          ]
+      ),
+
+    );
+  }
+}
