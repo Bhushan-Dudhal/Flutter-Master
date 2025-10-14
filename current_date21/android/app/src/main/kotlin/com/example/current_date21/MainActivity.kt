6fe140cee7ad27e13f754d423e24fc8d599b1f3b
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-10-14 20:26:31 +0530

    Current Date

---
+++ current_date21/android/app/src/main/kotlin/com/example/current_date21/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.current_date21
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ current_date21/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState(); 
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    var time = DateTime.now();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("Hello"), backgroundColor: Colors.amber),
+        body: Center(
+          child: Container(
+            width: 250,
+            height: 250,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Current Time: ${DateFormat('yQQQQ').format(time)}',
+                  style: TextStyle(fontSize: 20),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {}); 
+                  },
+                  child: Text("Refresh Time"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
