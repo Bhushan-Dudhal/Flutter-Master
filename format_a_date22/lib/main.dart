62999203a59a2d990273ca0bc41943d4ac6c327d
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-10-15 20:07:19 +0530

    Format a Date

---
+++ format_a_date22/lib/main.dart
@@ -0,0 +1,48 @@
+
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  State createState() {
+    return _MyApp();
+  }
+}
+
+class _MyApp extends State {
+  @override
+  Widget build(BuildContext context) {
+    var time = DateTime.now();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("App"), backgroundColor: Colors.amber),
+
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 200,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'current Time: ${DateFormat('yQQQQ').format(time)}',
+                  style: TextStyle(fontSize: 25),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {});
+                  },
+                  child: Text("welcome"),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
