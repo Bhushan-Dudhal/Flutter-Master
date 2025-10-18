00c514ee4c821d24fa334abb13b217ae02bb06a5
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-10-18 19:46:49 +0530

    Callback Function

---
+++ callback_function25/android/app/src/main/kotlin/com/example/callback_function25/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.callback_function25
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ callback_function25/lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "App",
+      theme: ThemeData(primarySwatch: Colors.deepOrange),
+      home: Screen(),
+    );
+  }
+}
+
+class Screen extends StatelessWidget {
+  callBack() {
+    print("call me");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        backgroundColor: Colors.deepPurpleAccent,
+        centerTitle: true,
+      ),
+
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            callBack();
+          },
+          child: Text('Click here'),
+        ),
+      ),
+    );
+  }
+}
