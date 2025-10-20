ccfd5d5b0730075a17a9ad0c0f77232832285636
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-10-20 21:35:13 +0530

    Stack Widget

---
+++ stack_widget27/android/app/src/main/kotlin/com/example/stack_widget27/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.stack_widget27
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ stack_widget27/lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Demo",
+    theme: ThemeData(
+      primarySwatch: Colors.cyan
+    ),
+    home:Screen() ,
+    );
+  }
+}
+
+class Screen extends StatelessWidget{
+
+  @override
+
+  Widget build(BuildContext context){
+
+    return Scaffold(
+      appBar: AppBar(title: Text(" Stack Widget"),
+      backgroundColor: Colors.redAccent,
+      centerTitle: true,
+      ),
+      body: Stack(
+        children: [
+          Container(
+            height: 200,
+            width: 200,
+            color: Colors.deepOrange,
+          ),
+            Container(
+            height: 160,
+            width: 160,
+            color: Colors.pink,
+          ),
+
+          Positioned(
+            top: 10,
+            left: 10,
+              child: Container(
+              height: 100,
+              width: 100,
+              color: Colors.purple,
+                        ),
+            )
+        ],
+      ),
+    );
+  }
+}
+
