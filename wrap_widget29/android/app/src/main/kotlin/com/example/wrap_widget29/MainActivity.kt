5ddbae6304eab94b0d1126c057669c1b532a454f
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-10-22 21:37:45 +0530

    Wrap Widget

---
+++ wrap_widget29/android/app/src/main/kotlin/com/example/wrap_widget29/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.wrap_widget29
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ wrap_widget29/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "APP",
+
+      theme: ThemeData(primarySwatch: Colors.cyan),
+      home: Screen(),
+    );
+  }
+}
+
+class Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        backgroundColor: Colors.deepPurpleAccent,
+        centerTitle: true,
+      ),
+      body: Container(
+        width: double.infinity,
+        child: Wrap(
+          direction: Axis.horizontal,
+          spacing: 11,
+          runSpacing: 11,
+          alignment: WrapAlignment.spaceEvenly,
+
+          children: [
+            Container(height: 70, width: 70, color: Colors.amberAccent),
+
+            Container(height: 70, width: 70, color: Colors.pink),
+            Container(height: 70, width: 70, color: Colors.orange),
+            Container(height: 70, width: 70, color: Colors.red),
+            Container(height: 70, width: 70, color: Colors.purple),
+
+            Container(height: 70, width: 70, color: Colors.amberAccent),
+
+            Container(height: 70, width: 70, color: Colors.pink),
+            Container(height: 70, width: 70, color: Colors.orange),
+            Container(height: 70, width: 70, color: Colors.red),
+            Container(height: 70, width: 70, color: Colors.purple),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
