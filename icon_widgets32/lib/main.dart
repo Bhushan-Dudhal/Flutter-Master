d7323c92a1f3310cb3f9b721d40c744d2f3f4110
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-10-28 21:22:09 +0530

    icon widgets updated

--- icon_widgets32/lib/main.dart
+++ icon_widgets32/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "App",
+      title: " new App",
       theme: ThemeData(primarySwatch: Colors.blueGrey),
       home: Screen(),
     );
