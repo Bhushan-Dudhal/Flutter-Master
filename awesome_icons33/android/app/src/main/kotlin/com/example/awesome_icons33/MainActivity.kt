dde6c29c28acd17b9f46a61da4397850543238f8
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-10-29 08:50:37 +0530

    Awesome Icons

---
+++ awesome_icons33/android/app/src/main/kotlin/com/example/awesome_icons33/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.awesome_icons33
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ awesome_icons33/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "App",
+      theme: ThemeData(primarySwatch: Colors.brown),
+      home: Screen(),
+    );
+  }
+}
+
+class Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext contect) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("home"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+      body: Text("Heloo"),
+    );
+  }
+}
