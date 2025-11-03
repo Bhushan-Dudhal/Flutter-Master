122249a9428ab68b1e4c83864e4d899feb76beed
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-11-03 17:55:04 +0530

    awesome icon file

--- awesome_icons33/lib/main.dart
+++ awesome_icons33/lib/main.dart
@@ -1,3 +1,38 @@
+// import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "App",
+//       theme: ThemeData(primarySwatch: Colors.brown),
+//       home: Screen(),
+//     );
+//   }
+// }
+// class Screen extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext contect) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text("home"),
+//         backgroundColor: Colors.amber,
+//         centerTitle: true,
+//       ),
+//       body:Container(
+//         child: Text("Welcome App"),
+
+//       )
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,26 +45,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "App",
-      theme: ThemeData(primarySwatch: Colors.brown),
-      home: Screen(),
+  home: Scaffold(
+    
+  ),
     );
   }
 }
-class Screen extends StatelessWidget {
-  @override
-  Widget build(BuildContext contect) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("home"),
-        backgroundColor: Colors.amber,
-        centerTitle: true,
-      ),
-      body:Container(
-        child: Text("Welcome App"),
-        
-      
-      )
-    );
-  }
-} 
