c73266a9502934081773e69ac12a1e23ab9490fd
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-11-11 10:04:53 +0530

    Positioned Widget

---
+++ positioned_widget34/android/app/src/main/kotlin/com/example/positioned_widget34/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.positioned_widget34
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ positioned_widget34/lib/main.dart
@@ -0,0 +1,54 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  build(BuildContext context) {
+    return MaterialApp(
+      title: "APP",
+
+      theme: ThemeData(shadowColor: Colors.blue),
+      home: Screen(),
+    );
+  }
+}
+
+class Screen extends StatelessWidget {
+  @override
+  build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text("Home"),
+        backgroundColor: Colors.deepOrangeAccent,
+        centerTitle: true,
+        ),
+
+        body: Container(
+            width:300,
+            height: 200,
+            color:CupertinoColors.darkBackgroundGray,
+            child:Stack(
+                children: [
+                   Positioned(
+                    bottom: 41,
+                    left: 41,
+
+                     child: Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.blueGrey,
+                     ),
+                   )
+                ],
+            )
+        ),
+    );
+  }
+}
+
