8b3962f43a514a4d71e1284f5bf49adc7a92e1df
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-11-16 17:23:00 +0530

    containerr fie updated

--- container01/lib/main.dart
+++ container01/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Welcome ',
+      title: 'Basic App ',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
       home: const MyHomePage(),
