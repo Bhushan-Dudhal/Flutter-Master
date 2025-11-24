7d08438881defe01df2d7f3517a92e8b9349c408
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-11-24 23:24:37 +0530

    Inkwell

--- inkwell_widget07/lib/main.dart
+++ inkwell_widget07/lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "Demo",
+    return MaterialApp(title: "Demo App",
      theme: ThemeData( 
       primarySwatch: Colors.deepOrange
      ),
