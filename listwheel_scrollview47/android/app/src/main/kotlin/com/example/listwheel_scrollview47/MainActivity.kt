4f1481dd6a17c2f75efa6ad2ad204bbd91375a6d
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-12-03 12:48:43 +0530

    ListWheel ScrollView Widget

---
+++ listwheel_scrollview47/android/app/src/main/kotlin/com/example/listwheel_scrollview47/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.listwheel_scrollview47
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ listwheel_scrollview47/lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListWeell();
+  }
+}
+
+class ListWeell extends StatefulWidget {
+  @override
+  State<ListWeell> createState() => ListWeelState();
+}
+
+class ListWeelState extends State<ListWeell> {
+  var nameIndex = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      body: ListWheelScrollView(
+        itemExtent: 100,
+        children: nameIndex
+            .map(
+              (val) => Container(
+                child: Text("$val"),
+                width: 400,
+                height: 200,
+                color: Colors.purpleAccent,
+              ),
+            )
+            .toList(),
+      ),
+    );
+  }
+}
