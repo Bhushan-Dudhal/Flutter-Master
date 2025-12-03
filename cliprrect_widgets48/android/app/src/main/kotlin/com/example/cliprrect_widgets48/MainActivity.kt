56437408e04dad1e1302c678ad2654832fc74880
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-12-03 15:06:01 +0530

    ClipRRect Widgets

---
+++ cliprrect_widgets48/android/app/src/main/kotlin/com/example/cliprrect_widgets48/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.cliprrect_widgets48
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ cliprrect_widgets48/lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Screen());
+  }
+}
+
+class Screen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Home"),
+      backgroundColor: Colors.deepPurpleAccent,
+      centerTitle: true,
+      ),
+      body: Center(
+        child: Container(
+          child: Column(
+            children: [
+              ClipRRect(
+                
+                // borderRadius: BorderRadius.only(topLeft: Radius.circular(22),bottomLeft: Radius.circular(22)),
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(20)),
+                child: Image.asset("assets/images/image.jpg"),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
