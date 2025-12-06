c638f2a6e4ae55e1d840acedf210d1a9f6e007da
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-12-06 06:00:39 +0530

    Ripple Animation

---
+++ ripple_animation_effects52/lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage>
+    with SingleTickerProviderStateMixin {
+  late Animation _animation;
+  late AnimationController _animationController;
+
+  var listRadius = [150.0, 200.0, 250.0, 300.0, 350.0];
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 5),
+    );
+    _animation = Tween(begin: 0.0, end: 1.0).animate(_animationController);
+
+    _animationController.addListener(() {
+      setState(() {
+        
+      });
+    });
+
+    _animationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home "),
+        backgroundColor: Colors.purpleAccent,
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
+          children: listRadius
+              .map(
+                (radius) => Container(
+                  width: radius * _animation.value,
+                  height: radius * _animation.value,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.purpleAccent.withOpacity(1.0 - _animation.value),
+                  ),
+                ),
+              )
+              .toList(),
+        ),
+      ),
+    );
+  }
+}
