7b458b3f61deb3b3a5a651b8a55a46d129d2f446
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-12-19 07:27:09 +0530

    Text Widget Updated

--- text_widgets03/lib/main.dart
+++ text_widgets03/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Demo App",
+      title: "Text Widget",
       theme: ThemeData(primarySwatch: Colors.cyan),
       home: _MyApp(),
     );
