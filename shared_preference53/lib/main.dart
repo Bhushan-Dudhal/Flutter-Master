b4da9e9c0c65d050ce0578fa31847c6d6e4ad79d
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-12-22 08:17:13 +0530

    shared preference updated

--- shared_preference53/lib/main.dart
+++ shared_preference53/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.deepPurple),
+      title: "Demo",
 
       home: HomePage(),
     
