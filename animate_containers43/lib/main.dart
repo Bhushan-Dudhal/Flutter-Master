4e211af7d73738873b6a88f7dff9a898a54c2d9d
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2025-12-29 09:43:04 +0530

    animated container update file

--- animate_containers43/lib/main.dart
+++ animate_containers43/lib/main.dart
@@ -12,6 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.red),
+      title: "Demo App",
       home: HomePage(),
     );
   }
