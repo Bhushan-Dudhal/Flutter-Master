bde092321c099c81ecb137a3f8465f7969d22a76
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2026-01-13 09:26:58 +0530

    noteapp using provider updated files

--- noteapp_using_provider56/lib/main.dart
+++ noteapp_using_provider56/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Flutter Demo",
+      title: "Demo App",
 
       
       debugShowCheckedModeBanner: false,
