f07c7118bc4e1d27fb2a98a04f280a8d4fe57246
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2026-01-25 00:02:33 +0530

    Wrap Widget

--- wrap_widget29/lib/main.dart
+++ wrap_widget29/lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Demo APP",
+      title:"DEMO APP",
 
       theme: ThemeData(primarySwatch: Colors.cyan),
       home: Screen(),
