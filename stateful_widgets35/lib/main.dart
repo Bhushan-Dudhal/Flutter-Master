949480612c2462ba4114ac78ec5545def4363b62
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2026-01-27 05:21:06 +0530

    stateful widget

--- stateful_widgets35/lib/main.dart
+++ stateful_widgets35/lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
 
       theme: ThemeData(primarySwatch: Colors.amber),
-
+       title: "Demo App",
       home: HomePage(),
     );
   }
