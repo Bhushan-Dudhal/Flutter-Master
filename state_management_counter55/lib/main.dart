7d27cf2fc1d3db12e59374fe5a11ccf046ff310c
Bhushan-Dudhal <bhushandudhal9@gmail.com>
2026-02-07 12:19:38 +0530

    state management updated

--- state_management_counter55/lib/main.dart
+++ state_management_counter55/lib/main.dart
@@ -13,6 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: "state management",
       home:ChangeNotifierProvider(create: (context)=>CounterProvider(),
       child: Home(),
       ),
